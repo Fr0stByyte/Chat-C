7d8f1db21bdf548be53fbf5646768950bdb13f93
Fr0stByyte <kroff396@outlook.com>
2025-12-22 00:35:20 -0600

    test vscode

---
+++ build/CMakeFiles/Chat_C.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kellen/Chat-C/Client/Client.c" "CMakeFiles/Chat_C.dir/Client/Client.c.o" "gcc" "CMakeFiles/Chat_C.dir/Client/Client.c.o.d"
+  "/home/kellen/Chat-C/Client/ClientSend.c" "CMakeFiles/Chat_C.dir/Client/ClientSend.c.o" "gcc" "CMakeFiles/Chat_C.dir/Client/ClientSend.c.o.d"
+  "/home/kellen/Chat-C/Client/TextProcessing.c" "CMakeFiles/Chat_C.dir/Client/TextProcessing.c.o" "gcc" "CMakeFiles/Chat_C.dir/Client/TextProcessing.c.o.d"
+  "/home/kellen/Chat-C/Server/Clients.c" "CMakeFiles/Chat_C.dir/Server/Clients.c.o" "gcc" "CMakeFiles/Chat_C.dir/Server/Clients.c.o.d"
+  "/home/kellen/Chat-C/Server/Commands.c" "CMakeFiles/Chat_C.dir/Server/Commands.c.o" "gcc" "CMakeFiles/Chat_C.dir/Server/Commands.c.o.d"
+  "/home/kellen/Chat-C/Server/IpList.c" "CMakeFiles/Chat_C.dir/Server/IpList.c.o" "gcc" "CMakeFiles/Chat_C.dir/Server/IpList.c.o.d"
+  "/home/kellen/Chat-C/Server/Requests.c" "CMakeFiles/Chat_C.dir/Server/Requests.c.o" "gcc" "CMakeFiles/Chat_C.dir/Server/Requests.c.o.d"
+  "/home/kellen/Chat-C/Server/Send.c" "CMakeFiles/Chat_C.dir/Server/Send.c.o" "gcc" "CMakeFiles/Chat_C.dir/Server/Send.c.o.d"
+  "/home/kellen/Chat-C/Server/Server.c" "CMakeFiles/Chat_C.dir/Server/Server.c.o" "gcc" "CMakeFiles/Chat_C.dir/Server/Server.c.o.d"
+  "/home/kellen/Chat-C/shared/Messages.c" "CMakeFiles/Chat_C.dir/shared/Messages.c.o" "gcc" "CMakeFiles/Chat_C.dir/shared/Messages.c.o.d"
+  "/home/kellen/Chat-C/shared/main.c" "CMakeFiles/Chat_C.dir/shared/main.c.o" "gcc" "CMakeFiles/Chat_C.dir/shared/main.c.o.d"
+  "" "Chat_C" "gcc" "CMakeFiles/Chat_C.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Chat_C.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chat_C.dir/link.d"
+  "CMakeFiles/Chat_C.dir/Client/Client.c.o"
+  "CMakeFiles/Chat_C.dir/Client/Client.c.o.d"
+  "CMakeFiles/Chat_C.dir/Client/ClientSend.c.o"
+  "CMakeFiles/Chat_C.dir/Client/ClientSend.c.o.d"
+  "CMakeFiles/Chat_C.dir/Client/TextProcessing.c.o"
+  "CMakeFiles/Chat_C.dir/Client/TextProcessing.c.o.d"
+  "CMakeFiles/Chat_C.dir/Server/Clients.c.o"
+  "CMakeFiles/Chat_C.dir/Server/Clients.c.o.d"
+  "CMakeFiles/Chat_C.dir/Server/Commands.c.o"
+  "CMakeFiles/Chat_C.dir/Server/Commands.c.o.d"
+  "CMakeFiles/Chat_C.dir/Server/IpList.c.o"
+  "CMakeFiles/Chat_C.dir/Server/IpList.c.o.d"
+  "CMakeFiles/Chat_C.dir/Server/Requests.c.o"
+  "CMakeFiles/Chat_C.dir/Server/Requests.c.o.d"
+  "CMakeFiles/Chat_C.dir/Server/Send.c.o"
+  "CMakeFiles/Chat_C.dir/Server/Send.c.o.d"
+  "CMakeFiles/Chat_C.dir/Server/Server.c.o"
+  "CMakeFiles/Chat_C.dir/Server/Server.c.o.d"
+  "CMakeFiles/Chat_C.dir/shared/Messages.c.o"
+  "CMakeFiles/Chat_C.dir/shared/Messages.c.o.d"
+  "CMakeFiles/Chat_C.dir/shared/main.c.o"
+  "CMakeFiles/Chat_C.dir/shared/main.c.o.d"
+  "Chat_C"
+  "Chat_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chat_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
